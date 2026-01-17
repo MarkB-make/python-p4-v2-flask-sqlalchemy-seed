23d5639e1ba09948d27edf1e66562dca1118e764
Mark <jadebrian999@gmail.com>
2026-01-17 14:30:36 +0300

    Initial commit

--- server/models.py
+++ server/models.py
@@ -1,14 +1,9 @@
+#!/usr/bin/env python3
+# server/models.py
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
-metadata = MetaData()
-
-# create the Flask SQLAlchemy extension
-db = SQLAlchemy(metadata=metadata)
-
-# define a model class by inheriting from db.Model.
-
+db = SQLAlchemy()
 
 class Pet(db.Model):
     __tablename__ = 'pets'
